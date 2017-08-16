cf6b3ed9ae08e334f375185ce15d2a263e9554ac
wuyukobe24 <jkwxq@sina.com>
2017-08-16 15:18:39 +0800

    Initial Commit

---
+++ 地址选择/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  地址选择
+//
+//  Created by WangXueqi on 17/8/16.
+//  Copyright © 2017年 JingBei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 地址选择/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  地址选择
+//
+//  Created by WangXueqi on 17/8/16.
+//  Copyright © 2017年 JingBei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
